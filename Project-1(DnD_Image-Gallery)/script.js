ee6740bb4ec304cd2b426d4e92853c2ce6f8a921
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-06-05 10:35:00 +0530

    Uploaded the images to the browser local storage...

--- Project-1(DnD_Image-Gallery)/script.js
+++ Project-1(DnD_Image-Gallery)/script.js
@@ -1,20 +1,26 @@
 const dropArea = document.getElementById("drop-area");
 const inputFile = document.getElementById("input-file");
 const imgView = document.getElementById("img-view");
+const uploadBtn = document.getElementById("upload-btn");
 
 inputFile.addEventListener("change", uploadImage);
 
-let imgLinkList = [];
+let imgLinkList = [] || JSON.parse(localStorage.getItem("imgLinkList"));
 
 function uploadImage() {
   const imageLink = URL.createObjectURL(inputFile.files[0]);
   imgLinkList.push(imageLink);
 
-  //   imgView.style.backgroundImage = `url(${imageLink})`;
   imgView.style.border = "none";
   imgView.style.textContent = "";
   imgView.style.padding = "";
   document.querySelector("#img-view img").src = imageLink;
+}
+
+uploadBtn.addEventListener("click", uploadImages);
+
+function uploadImages() {
+  localStorage.setItem("imgLinkList", JSON.stringify(imgLinkList));
 }
 
 dropArea.addEventListener("dragover", (e) => {
