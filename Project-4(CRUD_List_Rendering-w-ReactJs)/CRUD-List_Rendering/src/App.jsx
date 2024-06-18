c4cb871776c14ca992244bb23bba174de45b6922
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-06-18 21:10:40 +0530

    Added the list to our app...

--- Project-4(CRUD_List_Rendering-w-ReactJs)/CRUD-List_Rendering/src/App.jsx
+++ Project-4(CRUD_List_Rendering-w-ReactJs)/CRUD-List_Rendering/src/App.jsx
@@ -1,22 +1,38 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [formText, setformText] = useState({
     text: "",
   });
+
+  const [listItems, setlistItems] = useState(
+    JSON.parse(localStorage.getItem("listItems")) || []
+  );
+
+  useEffect(() => {
+    console.log(listItems);
+    localStorage.setItem("listItems", JSON.stringify(listItems));
+  }, [listItems]);
+
   const inputHandler = (e) => {
-    // const { text } = e.target;
     setformText({
       ...formText,
       [e.target.name]: e.target.value,
     });
     // console.log(e.target);
   };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setlistItems([...listItems, formText.text]);
+    // console.log(listItems);
+  };
+
   return (
     <>
       <div className="h-[100vh] w-full bg-red-400 flex items-center justify-center">
         <div className="w-1/3 h-2/3 bg-white p-5 shadow-xl shadow-red-600 flex flex-col gap-2">
-          <form className="flex justify-between">
+          <form className="flex justify-between" onSubmit={submitHandler}>
             <input
               type="text"
               name="text"
@@ -32,9 +48,18 @@ function App() {
               Submit
             </button>
           </form>
-          <div className="w-full h-full border-2 border-black text-center flex flex-col gap-1">
+          <div className="w-full h-full text-center flex flex-col gap-1  bg-slate-200">
             <h1 className="font-bold text-xl font-serif">List-Items</h1>
-            <ul className=""></ul>
+            <ul className="flex flex-col gap-2">
+              {listItems?.map((item, i) => (
+                <li
+                  key={i}
+                  className="w-full py-3 bg-white shadow-xl shadow-black"
+                >
+                  {item}
+                </li>
+              ))}
+            </ul>
           </div>
         </div>
       </div>
