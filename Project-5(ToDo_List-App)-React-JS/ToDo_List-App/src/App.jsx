7538ee4efa3e3cc8c8408a25ad66ea23c5602164
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-08-14 19:38:10 +0530

    Setup the project codebase...

---
+++ Project-5(ToDo_List-App)-React-JS/ToDo_List-App/src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <>
+      <div className="bg-primary w-full h-screen flex flex-col items-center p-16 gap-5">
+        <h1 className="font-bold text-2xl italic text-secondary">TODO-APP</h1>
+
+        <div className="w-72 p-10 border-dark border-4 rounded-xl">
+          {/* <h2>ksfgklfngkln</h2> */}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
